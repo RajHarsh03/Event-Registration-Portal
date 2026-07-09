c9180d8cd5672ec31105d4c4f1b6a232718ed93c
Harsh Raj <raj.harsh4618@gmail.com>
2026-07-09 12:33:29 +0530

    feat: implement EventRegistration component with form, validation, table and delete

--- src/app/components/event-registration/event-registration.component.ts
+++ src/app/components/event-registration/event-registration.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EventService } from '../../services/event.service';
+import { Registration } from '../../models/registration.model';
 
 @Component({
   selector: 'app-event-registration',
@@ -6,4 +9,46 @@ import { Component } from '@angular/core';
   templateUrl: './event-registration.component.html',
   styleUrl: './event-registration.component.css',
 })
-export class EventRegistrationComponent {}
+export class EventRegistrationComponent implements OnInit {
+  registrationForm!: FormGroup;
+  registrations: Registration[] = [];
+
+  constructor(private fb: FormBuilder, private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      participantName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      eventName: ['', Validators.required],
+      mobileNumber: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]{10}$')],
+      ],
+    });
+
+    this.loadRegistrations();
+  }
+
+  // Convenience getters for template access
+  get participantName() { return this.registrationForm.get('participantName'); }
+  get email() { return this.registrationForm.get('email'); }
+  get eventName() { return this.registrationForm.get('eventName'); }
+  get mobileNumber() { return this.registrationForm.get('mobileNumber'); }
+
+  loadRegistrations(): void {
+    this.registrations = this.eventService.getRegistrations();
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.invalid) return;
+
+    this.eventService.addRegistration(this.registrationForm.value);
+    this.loadRegistrations();
+    this.registrationForm.reset();
+  }
+
+  onDelete(id: number): void {
+    this.eventService.deleteRegistration(id);
+    this.loadRegistrations();
+  }
+}
